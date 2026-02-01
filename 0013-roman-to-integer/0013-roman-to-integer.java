41cb2cece42ef9ce18903a2618e84e0d361021e2
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-02-01 23:05:46 +0530

    Time: 2 ms (99.44%), Space: 46.6 MB (46.33%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,23 +1,93 @@
 class Solution {
     public int romanToInt(String s) {
-        int res = 0;
-        Map<Character, Integer> roman = new HashMap<>();
-        roman.put('I', 1);
-        roman.put('V', 5);
-        roman.put('X', 10);
-        roman.put('L', 50);
-        roman.put('C', 100);
-        roman.put('D', 500);
-        roman.put('M', 1000);
 
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
-                res -= roman.get(s.charAt(i));
-            } else {
-                res += roman.get(s.charAt(i));
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            switch (c) {
+
+                case 'I':
+
+                    if (i == s.length() - 1) {
+                        sum += 1;
+                        break;
+                    }
+                    if (s.charAt(i + 1) == 'V') {
+                        sum += 4;
+                        i++;
+                        break;
+                    }
+                    if (s.charAt(i + 1) == 'X') {
+                        sum += 9;
+                        i++;
+                        break;
+                    }
+
+                    sum += 1;
+                    break;
+
+                case 'V':
+                    sum += 5;
+                    break;
+
+                case 'X':
+                    if (i == s.length() - 1) {
+                        sum += 10;
+                        break;
+                    }
+                    if (s.charAt(i + 1) == 'L') {
+                        sum += 40;
+                        i++;
+                        break;
+                    }
+                    if (s.charAt(i + 1) == 'C') {
+                        sum += 90;
+                        i++;
+                        break;
+                    }
+                    sum += 10;
+                    break;
+
+                case 'L':
+                    sum += 50;
+                    break;
+
+                case 'C':
+                    if (i == s.length() - 1) {
+                        sum += 100;
+                        break;
+                    }
+                    if (s.charAt(i + 1) == 'D') {
+                        sum += 400;
+                        i++;
+                        break;
+                    }
+                    if (s.charAt(i + 1) == 'M') {
+                        sum += 900;
+                        i++;
+                        break;
+                    }
+                    sum += 100;
+                    break;
+
+                case 'D':
+                    sum += 500;
+                    break;
+
+                case 'M':
+                    sum += 1000;
+                    break;
+
+                default:
+                    break;
+
             }
+
         }
 
-        return res + roman.get(s.charAt(s.length() - 1));        
+        return sum;
+
     }
 }
