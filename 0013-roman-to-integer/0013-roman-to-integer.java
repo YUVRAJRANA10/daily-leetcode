3a2fdc39f1b491adf649a4780b90b190d11b5795
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-02-01 22:53:50 +0530

    Time: 2 ms (99.44%), Space: 46.6 MB (59.15%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,103 @@
+class Solution {
+    public int romanToInt(String s) {
+        
+int sum = 0;
+//    for(char c : s.toCharArray()){
+    
+
+
+
+//    }
+
+
+for(int i = 0; i < s.length() ; i++){
+char c = s.charAt(i);
+
+
+         switch(c){
+
+
+        case 'I':
+
+        if(i == s.length() - 1){
+            sum+= 1;
+            break;
+         }
+        if(s.charAt(i+1) == 'V'){
+            sum+= 4;
+            i++;
+            break;
+        }
+        if(s.charAt(i+1) == 'X'){
+            sum+= 9;
+            i++;
+            break;
+        }
+
+          sum+= 1;
+           break;
+
+        case 'V':
+        sum+= 5;
+           break;
+        
+        case 'X':
+        if(i == s.length() - 1){
+            sum+= 10;
+            break;
+         }        
+        if(s.charAt(i+1) == 'L'){
+            sum+= 40;
+            i++;
+            break;
+        }
+        if(s.charAt(i+1) == 'C'){
+            sum+= 90;
+            i++;
+            break;
+        }                
+         sum+= 10;
+           break;
+        
+        case 'L':
+        sum+= 50;
+           break;
+
+        case 'C':
+        if(i == s.length() - 1){
+            sum+= 100;
+            break;
+         }        
+        if(s.charAt(i+1) == 'D'){
+            sum+= 400;
+            i++;
+            break;
+        }
+        if(s.charAt(i+1) == 'M'){
+            sum+= 900;
+            i++;
+            break;
+        }                
+         sum+=100;
+           break;
+
+        case 'D':
+        sum+=500;
+           break;
+
+        case 'M':
+        sum+= 1000;
+           break;
+
+     default:    
+        break;
+
+
+     }
+
+}
+
+   return sum;
+
+    }
+}
