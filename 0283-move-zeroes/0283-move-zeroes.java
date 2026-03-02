7dd915939a569e962889eb4c3c03e64dde785378
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-03-02 19:44:17 +0530

    Time: 2 ms (90.15%), Space: 47.3 MB (98.95%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+                int[] arr2 = new int[arr.length];
+int ptr = 0;
+          for(int i = 0; i < arr.length ; i++){
+           if(arr[i] != 0){
+            arr2[ptr] = arr[i];
+            ptr++;
+           }
+          }
+
+           for(int j = ptr; j < arr2.length; j++){
+            arr2[j] = 0;
+           }
+           
+
+           for(int i = 0; i < arr.length; i++){
+            arr[i] = arr2[i];
+           }
+    }
+}
