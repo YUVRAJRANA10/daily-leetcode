d22b88404bc0a46fbc93f1f856c309d9f32fdab6
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-03-03 16:28:39 +0530

    Time: 2 ms (90.21%), Space: 48 MB (36.51%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,21 +1,34 @@
 class Solution {
     public void moveZeroes(int[] arr) {
-                int[] arr2 = new int[arr.length];
-int ptr = 0;
-          for(int i = 0; i < arr.length ; i++){
-           if(arr[i] != 0){
-            arr2[ptr] = arr[i];
-            ptr++;
-           }
-          }
+//                 int[] arr2 = new int[arr.length];
+// int ptr = 0;
+//           for(int i = 0; i < arr.length ; i++){
+//            if(arr[i] != 0){
+//             arr2[ptr] = arr[i];
+//             ptr++;
+//            }
+//           }
 
-           for(int j = ptr; j < arr2.length; j++){
-            arr2[j] = 0;
-           }
+//            for(int j = ptr; j < arr2.length; j++){
+//             arr2[j] = 0;
+//            }
            
 
-           for(int i = 0; i < arr.length; i++){
-            arr[i] = arr2[i];
+//            for(int i = 0; i < arr.length; i++){
+//             arr[i] = arr2[i];
+//            }
+
+            int ptr = 0;
+
+        for(int i = 0; i < arr.length  ; i++){
+            
+           if(arr[i] != 0){
+            int temp = arr[ptr];
+            arr[ptr] = arr[i];
+            arr[i] = temp;
+            ptr++;
            }
+
+          }
     }
 }
