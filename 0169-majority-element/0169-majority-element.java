fc6d25e31c218e48012d32da949b3b32965bc826
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-03-06 18:10:08 +0530

    Time: 17 ms (15.46%), Space: 52.6 MB (90.91%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        int n = nums.length / 2;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.merge(nums[i], 1, Integer::sum);
+
+            if (map.get(nums[i]) > n) {
+                return nums[i];
+            }
+
+        }
+
+        return -1;
+
+    }
+}
