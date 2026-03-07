eee4a570b282d7f5861986b6e8b765bbced58875
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-03-08 03:42:50 +0530

    Time: 1 ms (79.25%), Space: 46.7 MB (61.76%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
+    public int removeDuplicates(int[] arr) {
 
 //         LinkedHashSet<Integer> set = new LinkedHashSet<>();
        
@@ -21,17 +21,36 @@ class Solution {
 //         return count;
 
 
-        if (nums.length == 0) return 0;
+    //     if (nums.length == 0) return 0;
 
-        int k = 1; 
+    //     int k = 1; 
 
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i - 1]) {
-                nums[k] = nums[i];
-                k++;
-            }
-        }
-        return k;
+    //     for (int i = 1; i < nums.length; i++) {
+    //         if (nums[i] != nums[i - 1]) {
+    //             nums[k] = nums[i];
+    //             k++;
+    //         }
+    //     }
+    //     return k;
+    // }
+
+
+int i = 0;
+int j = 1 ;
+while( j <=arr.length - 1){
+
+if(arr[j] != arr[i]){
+    
+        arr[i+1] = arr[j];
+    
+  i++;
+}
+
+j++;
+}
+
+
+return i+1;
+
     }
-
-    }
+}
