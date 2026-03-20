0e0839b2cd7195dc3709c56cf1636fb5b96ee3d4
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-03-20 15:25:11 +0530

    Time: 0 ms (100%), Space: 47.6 MB (89.38%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        if (N == 0) return 1;
+
+        double half = myPow(x, (int)(N / 2));
+
+        if (N % 2 == 0)
+            return half * half;
+        else
+            return x * half * half;
+    }
+}
