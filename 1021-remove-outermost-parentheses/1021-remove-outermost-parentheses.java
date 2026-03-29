0c5701d59ef08f65dbc8e0fe3dbc14934b8459e6
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-03-29 23:20:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,35 +1,62 @@
 class Solution {
     public String removeOuterParentheses(String expr) {
 
-
-       Stack<Character> st = new Stack<>();
+    //      #1 approach
+    //    Stack<Character> st = new Stack<>();
 
         StringBuilder sb = new StringBuilder();
+    //     for(char c : expr.toCharArray()){
+    //       if(st.isEmpty() && c == '('){
+    //         st.push(c);
+    //         continue;
+    //       }
+    //       else if(!st.isEmpty() && c == '('){
+    //              sb.append(c);
+    //              st.push(c);
+    //           }
+    //              else{
+                    
+    //                 st.pop();
+
+    //                 if(!st.isEmpty()){
+    //                     sb.append(c);
+    //                 }
+
+    //               }
+              
+            
+    //       }
+
+
+
+    //       return sb.toString();
+
+
+        int counter = 0;
         for(char c : expr.toCharArray()){
-          if(st.isEmpty() && c == '('){
-            st.push(c);
+   
+          if(c == '('){
+             if(counter == 0){
+                counter++;
             continue;
           }
-          else if(!st.isEmpty() && c == '('){
-                 sb.append(c);
-                 st.push(c);
-              }
-                 else{
-                    
-                    st.pop();
-
-                    if(!st.isEmpty()){
-                        sb.append(c);
-                    }
-
-                  }
+            counter++;
+            sb.append(c);
+          }
+          else if(c == ')'){
+            counter--;
+             if(counter == 0){
+            continue;
+          }
+            sb.append(c);
+          }
               
             
           }
 
 
 
-          return sb.toString();
+      return sb.toString();
       }
        
         
