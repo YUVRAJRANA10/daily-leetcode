b3a702cea69df933f0c1a2e214fdeb752a1219c1
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-04-05 12:47:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -14,26 +14,33 @@ class Solution {
             return n;
         }
 
-        for(int i = 1; i < n; i++){
-            if(nums[i] > nums[i-1] && nums[i] > nums[i+1]){
-                return i;
+        // for(int i = 1; i < n; i++){
+        //     if(nums[i] > nums[i-1] && nums[i] > nums[i+1]){
+        //         return i;
+        //     }
+        // }
+
+        // return -1;
+
+        int left = 1;
+        int right = n - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if(nums[mid] > nums[mid -1] && nums[mid] > nums[mid+1]){
+                return mid;
             }
+
+            
+           if(nums[mid] < nums[mid+ 1]){
+            left = mid + 1;
+           }
+           else{
+            right =mid -1;
+           }
         }
 
         return -1;
-
-        // int left = 1;
-        // int right = n - 1;
-
-        // while (left <= right) {
-        //     int mid = (left + right) / 2;
-
-        //     if(nums[mid] > nums[mid -1] && nums[mid] > nums[mid+1]){
-        //         return mid;
-        //     }
-
-            
-
-        // }
     }
 }
