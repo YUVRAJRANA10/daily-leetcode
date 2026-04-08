0817ff3f8c35f34d7232629ad4b8204f1b2c0a15
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-04-08 21:58:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,24 +1,13 @@
-import java.util.*;
-
 class Solution {
     public int missingNumber(int[] nums) {
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        // store frequency
-        for (int num : nums) {
-            map.put(num, 1);
-        }
-        
+        int xor = 0;
         int n = nums.length;
-        
-        // check from 0 to n
-        for (int i = 0; i <= n; i++) {
-            if (!map.containsKey(i)) {
-                return i;
-            }
+
+        for (int i = 0; i < n; i++) {
+            xor ^= nums[i];
+            xor ^= i;
         }
-        
-        return -1; // never happens
+
+        return xor ^ n;
     }
 }
