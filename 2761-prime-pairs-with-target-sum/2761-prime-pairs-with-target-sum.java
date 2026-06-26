2e19329331691f6e2d8871e1930e3f049f9256ad
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-06-26 14:39:13 +0530

    Time: 501 ms (17.87%), Space: 277.7 MB (5.01%) - LeetHub

--- 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.java
+++ 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.java
@@ -1,70 +1,61 @@
 class Solution {
     int size = 1000000;
     int prime[] = new int[size + 1];
-    
 
-    public void prime(int n){
-         Arrays.fill(prime,1);
-    prime[0]=0;
-    prime[1]=0;
-         for(int i = 2; i*i < n;i++ ){
+    public void prime(int n) {
+        Arrays.fill(prime, 1);
+        prime[0] = 0;
+        prime[1] = 0;
+        for (int i = 2; i * i < n; i++) {
 
-            if(prime[i] == 1){
+            if (prime[i] == 1) {
 
-                for(int j = i * i;j < n;j+=i){
+                for (int j = i * i; j < n; j += i) {
                     prime[j] = 0;
                 }
             }
-         }
-
+        }
 
     }
 
     public List<List<Integer>> findPrimePairs(int n) {
         prime(n);
         int x = 2;
-        int y = n-1;
-List<List<Integer>> l1 = new ArrayList<>();
-        while(x<=y){
+        int y = n - 1;
+        List<List<Integer>> l1 = new ArrayList<>();
+        while (x <= y) {
 
-          List<Integer> l2 = new ArrayList<>();
-             if(prime[x] == 1 && prime[y] == 1){
-                
-                if(x+y == n){
-                   l2.add(x);
-                   l2.add(y);
-                   l1.add(l2);
-                   x++;
-                   y--;
-                   continue;
-                }
-                else if(x+y<n){
+            List<Integer> l2 = new ArrayList<>();
+            if (prime[x] == 1 && prime[y] == 1) {
+
+                if (x + y == n) {
+                    l2.add(x);
+                    l2.add(y);
+                    l1.add(l2);
                     x++;
-                }
-                else if(x+y>n){
+                    y--;
+                    continue;
+                } else if (x + y < n) {
+                    x++;
+                } else if (x + y > n) {
                     y--;
                 }
 
-             }
-             else{
-             if(prime[x] == 1 && prime[y] == 0){
-             y--;   
+            } else {
+                if (prime[x] == 1 && prime[y] == 0) {
+                    y--;
 
-             }
-            else if(prime[x] == 0 && prime[y] == 1){
-                x++;
+                } else if (prime[x] == 0 && prime[y] == 1) {
+                    x++;
 
-             }
-             else{
-                x++;
-                y--;
-             }
-             }
+                } else {
+                    x++;
+                    y--;
+                }
+            }
 
-           
         }
 
-        
-return l1;
+        return l1;
     }
 }
