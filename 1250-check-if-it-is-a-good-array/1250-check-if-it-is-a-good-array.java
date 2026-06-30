d2b991f99e5e04f88e6a67ead3c422be3cb2316b
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-06-30 09:43:11 +0530

    Time: 3 ms (85.42%), Space: 62.3 MB (6.48%) - LeetHub

---
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.java
@@ -0,0 +1,27 @@
+class Solution {
+
+
+    
+    int gcd( int a, int b){
+
+        while(b != 0){
+
+            int rem = a % b;
+            a = b;
+            b = rem;
+        }
+
+        return a;
+}
+    public boolean isGoodArray(int[] nums) {
+        
+  int gcd = nums[0];
+for(int i = 1; i < nums.length; i++){
+
+
+    gcd = gcd(gcd , nums[i]);
+}
+    return gcd==1;
+
+    }
+}
