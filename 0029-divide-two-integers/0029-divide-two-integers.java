782c982c6c5836efdeb54b49b1f33c4f135e3765
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-07-17 15:22:56 +0530

    Time: 2372 ms (3.08%), Space: 42.9 MB (13.95%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -5,6 +5,26 @@ class Solution {
             return Integer.MAX_VALUE;
         }
 
-        return dividend / divisor;
+        if(divisor == 1) return dividend;
+        if(divisor == -1)return -dividend;
+
+        boolean negative = false;
+        if((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)){
+            negative = true;
+        }
+     
+      long a = Math.abs((long)dividend);
+      long b = Math.abs((long)divisor);
+
+      int ans = 0;
+      while(a>=b){
+
+        a = a-b;
+        ans++;
+      }
+
+      return negative ? -ans : ans;
+
+
     }
 }
