5f29d1f4cbb2e021950197dae1251bf1e3f8106c
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-07-20 14:45:44 +0530

    Time: 226 ms (29.03%), Space: 197.3 MB (5.12%) - LeetHub

--- 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.java
+++ 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.java
@@ -1,61 +1,64 @@
 class Solution {
-    int size = 1000000;
-    int prime[] = new int[size + 1];
+    int hash[];
+    public int[] skewprime(int n){
 
-    public void prime(int n) {
-        Arrays.fill(prime, 1);
-        prime[0] = 0;
-        prime[1] = 0;
-        for (int i = 2; i * i < n; i++) {
+        int size = n+1;
+         hash = new int[size];
+        Arrays.fill(hash,1);
+        hash[0] = 0;
+        hash[1] = 0;
 
-            if (prime[i] == 1) {
 
-                for (int j = i * i; j < n; j += i) {
-                    prime[j] = 0;
+        for(int i = 2 ; i * i < n;i++){
+
+            if(hash[i] == 1){
+                for(int j = i*i; j < n; j+=i){
+                    hash[j] = 0;
                 }
             }
         }
 
+        return hash;
     }
-
     public List<List<Integer>> findPrimePairs(int n) {
-        prime(n);
+        
+      skewprime(n);
+
+        List<List<Integer>> aa = new ArrayList<>();
         int x = 2;
-        int y = n - 1;
-        List<List<Integer>> l1 = new ArrayList<>();
-        while (x <= y) {
+        int y = n -1;
+    
+          
+          while(x<=y){
+        
 
-            List<Integer> l2 = new ArrayList<>();
-            if (prime[x] == 1 && prime[y] == 1) {
-
-                if (x + y == n) {
-                    l2.add(x);
-                    l2.add(y);
-                    l1.add(l2);
-                    x++;
-                    y--;
-                    continue;
-                } else if (x + y < n) {
+          if(hash[x] == 1 && hash[y] == 1){
+             List<Integer> a = new ArrayList<>();
+               if(x+y == n){
+                a.add(x);
+                a.add(y);
+                aa.add(a);
+                x++;
+                y--;
+               }
+                else if (x + y < n) {
                     x++;
                 } else if (x + y > n) {
                     y--;
                 }
+          
+          }
+          else if(hash[x] == 1 && hash[y] == 0)y--;
+          else if(hash[x] == 0 && hash[y] == 1)x++;
+          else{
+            x++;
+            y--;
+          }
 
-            } else {
-                if (prime[x] == 1 && prime[y] == 0) {
-                    y--;
 
-                } else if (prime[x] == 0 && prime[y] == 1) {
-                    x++;
+          }
+return aa;
+  
 
-                } else {
-                    x++;
-                    y--;
-                }
-            }
-
-        }
-
-        return l1;
     }
 }
