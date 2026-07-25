6c8ed80e89afedcbcdc78e2bc903345d92ac41ce
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-07-25 11:32:44 +0530

    Time: 17 ms (16.48%), Space: 52.6 MB (85.49%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -6,7 +6,8 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            map.merge(nums[i], 1, Integer::sum);
+            // map.merge(nums[i], 1, Integer::sum);
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
 
             if (map.get(nums[i]) > n) {
                 return nums[i];
