0984b910092d0d5045215f14b2e37837d2e2e6ae
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-07-30 15:53:36 +0530

    Time: 3 ms (88.48%), Space: 48.2 MB (65.31%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,31 +1,39 @@
 class Solution {
-    public boolean bs(int matrix[],int target){
-
-         int left = 0;
-         int right = matrix.length -1;
-         while(left <= right){
-
-            int mid = (left+right)/2;
-           if(matrix[mid] == target)return true;
-            if(target < matrix[mid]){
-                right = mid -1;
-            }
-            else{
-                left = mid+1;
-            }
-         }
-         return false;
-    }
+    // public boolean bs(int matrix[],int target){
+    //      int left = 0;
+    //      int right = matrix.length -1;
+    //      while(left <= right){
+    //         int mid = (left + right) / 2;
+    //        if(matrix[mid] == target) return true;
+    //         if(target < matrix[mid]){
+    //             right = mid -1;
+    //         }
+    //         else{
+    //             left = mid+1;
+    //         }
+    //      }
+    //      return false;
+    // }
     public boolean searchMatrix(int[][] matrix, int target) {
-
         int row = matrix.length;
         int col = matrix[0].length;
+         
+          int l = 0, r = col -1;
 
-        for(int i = 0; i < row; i++){
-            if(target >= matrix[i][0] && target <= matrix[i][col-1]){
-            if(bs(matrix[i],target))return true;}
-        }
-        return false;
+          while(l < row && r >= 0){
+
+            if(matrix[l][r] == target)return true;
+
+            if(matrix[l][r] < target){
+             
+                l++;
+
+            }
+            else{
+                r--;
+            }
+          }
+          return false;
         
     }
 }
