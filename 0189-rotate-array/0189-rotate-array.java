956325972b4ef356a5687a708e8c861ba64a9d33
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-08-03 15:18:28 +0530

    Time: 1 ms (41.15%), Space: 63.2 MB (17.02%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -13,4 +13,27 @@ class Solution {
             nums[i] = temp[i];
         }
     }
+
+    //     int n = nums.length;
+    //     k = k % n;   // very important
+
+    //     // Step 1: reverse whole array
+    //     reverse(nums, 0, n - 1);
+
+    //     // Step 2: reverse first k elements
+    //     reverse(nums, 0, k - 1);
+
+    //     // Step 3: reverse remaining elements
+    //     reverse(nums, k, n - 1);
+    // }
+
+    // private void reverse(int[] nums, int start, int end) {
+    //     while (start < end) {
+    //         int temp = nums[start];
+    //         nums[start] = nums[end];
+    //         nums[end] = temp;
+    //         start++;
+    //         end--;
+    //     }
+    // }
 }
