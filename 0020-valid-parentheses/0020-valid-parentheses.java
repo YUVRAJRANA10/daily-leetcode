1297e7d5e70d8f7329ca7dded00c52d2e0f1b34a
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-08-04 23:03:51 +0530

    Time: 3 ms (86.06%), Space: 43.3 MB (41.52%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,20 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
-    //     int open = 0;
-    //     int close = 0;
-    //   for(char c : s.toCharArray()){
 
-    //     if( c == '{' || c == '(' || c == '['){
-    //       open++;
-    //     }
-    //     else{
-    //         if(open > close){
-
-                       
-    //             return 
-    //         }
-
-    //     }
         Stack<Character> stack = new Stack<>(); // Declare a character stack
 
         for (char c : s.toCharArray()) {
@@ -22,22 +8,22 @@ class Solution {
             if (c == '(' || c == '[' || c == '{') {
                 stack.push(c);
             } 
-            // If a closing bracket, pop from stack and check for a match
-            else if (c == ')') {
-                if (stack.isEmpty() || stack.pop() != '(') return false;
-            } 
-            else if (c == ']') {
-                if (stack.isEmpty() || stack.pop() != '[') return false;
-            } 
-            else if (c == '}') {
-                if (stack.isEmpty() || stack.pop() != '{') return false;
+            else{
+                   
+             if(!stack.isEmpty()){
+                 
+                   if(c == ')' && stack.pop() != '(')return false;
+                   else if(c == ']' && stack.pop() != '[')return false;
+                   else if(c == '}' && stack.pop() != '{')return false;
+
+             }
+             else{
+                return false;
+             }
+
             }
+        
         }
-
-
-
-
-      
 
       return stack.isEmpty();
 
