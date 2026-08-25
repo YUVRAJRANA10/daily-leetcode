6f7b656502feeb87dbf0c44cdee9cf94376336dc
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-08-25 10:17:39 +0530

    Time: 2 ms (60.17%), Space: 46.3 MB (42.66%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        
+        int arr[] = new int[1001];
+
+        for(int i = 0; i < trips.length; i++){
+            
+
+          arr[trips[i][1]] += trips[i][0];
+          arr[trips[i][2]] -= trips[i][0];
+
+
+        }
+
+        int passangers = 0;
+
+        for(int i =0 ; i <  arr.length ; i++){
+
+        if(passangers > capacity){
+            return false;
+        }
+
+        passangers += arr[i];
+        }
+        return true;
+
+        
+    }
+
+
+}
