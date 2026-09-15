a7c28248f21b742961b0d0d8bfbb28ec86844d11
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-09-15 22:29:36 +0530

    Time: 102 ms (0.91%), Space: 44.2 MB (56.9%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,24 +1,22 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        int n = nums.length;
-        List<List<Integer>> allSubsets = new ArrayList<>();
-        
-        // Loop from 0 to (2^n) - 1
-        for(int i = 0; i < (1 << n); i++) {
-            List<Integer> currentSubset = new ArrayList<>();
-            
-            // Check each bit of 'i' from 0 to n-1
-        for(int j = 0 ; j < n; j++){
 
-            if((i & (1 << j)) != 0){
-                currentSubset.add(nums[j]);
-            }
+    public void generate(int index, List<Integer> curr, List<List<Integer>> ans, int[] arr) {
+        if (index == arr.length) {
+            ans.add(new ArrayList<>(curr));
+            return;
         }
-            
-            // Add the constructed subset to our main list
-            allSubsets.add(currentSubset);
-        }
-        
-        return allSubsets;
+        curr.add(arr[index]);
+        generate(index + 1, curr, ans, arr);
+        curr.remove(curr.size() - 1);
+        generate(index + 1, curr, ans, arr);
+
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        generate(0, new ArrayList<>(), ans, nums);
+        return ans;
     }
 }
