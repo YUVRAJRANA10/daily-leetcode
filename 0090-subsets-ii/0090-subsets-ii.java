e5c917d0a47da36d9576b605154548b2332ee4f4
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-09-16 09:59:44 +0530

    Time: 3 ms (53.55%), Space: 44.8 MB (94.61%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,25 +1,51 @@
+// class Solution {
+
+//     public void solve(int start, int nums[], List<Integer> path, List<List<Integer>> ans) {
+//         ans.add(new ArrayList<>(path));
+//         for (int i = start; i < nums.length; i++) {
+
+//             if (i > start && nums[i] == nums[i - 1]) {
+//                 continue;
+//             }
+//             path.add(nums[i]);
+
+//             solve(i + 1, nums, path, ans);
+
+//             path.remove(path.size() - 1);
+
+//         }
+//     }
+
+//     public List<List<Integer>> subsetsWithDup(int[] nums) {
+//         Arrays.sort(nums);
+//         List<List<Integer>> ans = new ArrayList<>();
+//         solve(0, nums, new ArrayList<>(), ans);
+//         return ans;
+//     }
+// }
+
+
 class Solution {
 
-    public void solve(int start, int nums[], List<Integer> path, List<List<Integer>> ans) {
-        ans.add(new ArrayList<>(path));
-        for (int i = start; i < nums.length; i++) {
-
-            if (i > start && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            path.add(nums[i]);
-
-            solve(i + 1, nums, path, ans);
-
-            path.remove(path.size() - 1);
-
+    public void generate(int index, List<Integer> curr,    HashSet<List<Integer>> ans, int[] arr) {
+        if (index == arr.length) {
+            ans.add(new ArrayList<>(curr));
+            return;
         }
+        curr.add(arr[index]);
+        generate(index + 1, curr, ans, arr);
+        curr.remove(curr.size() - 1);
+        generate(index + 1, curr, ans, arr);
+
     }
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        solve(0, nums, new ArrayList<>(), ans);
-        return ans;
+
+        Arrays.sort(nums);
+        HashSet<List<Integer>> set = new HashSet<>();
+        generate(0, new ArrayList<>(), set, nums);
+
+   
+        return new ArrayList<>(set);
     }
 }
