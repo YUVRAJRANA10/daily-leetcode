9036b46de5ee2ee65e00bbb458c28ed2e27cdfac
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-09-16 10:39:02 +0530

    Time: 2 ms (99.73%), Space: 44.9 MB (88.73%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,33 +1,6 @@
-// class Solution {
-
-//     public void solve(int start, int nums[], List<Integer> path, List<List<Integer>> ans) {
-//         ans.add(new ArrayList<>(path));
-//         for (int i = start; i < nums.length; i++) {
-
-//             if (i > start && nums[i] == nums[i - 1]) {
-//                 continue;
-//             }
-//             path.add(nums[i]);
-
-//             solve(i + 1, nums, path, ans);
-
-//             path.remove(path.size() - 1);
-
-//         }
-//     }
-
-//     public List<List<Integer>> subsetsWithDup(int[] nums) {
-//         Arrays.sort(nums);
-//         List<List<Integer>> ans = new ArrayList<>();
-//         solve(0, nums, new ArrayList<>(), ans);
-//         return ans;
-//     }
-// }
-
-
 class Solution {
 
-    public void generate(int index, List<Integer> curr,    HashSet<List<Integer>> ans, int[] arr) {
+    public void generate(int index, List<Integer> curr, List<List<Integer>> ans, int[] arr) {
         if (index == arr.length) {
             ans.add(new ArrayList<>(curr));
             return;
@@ -35,17 +8,43 @@ class Solution {
         curr.add(arr[index]);
         generate(index + 1, curr, ans, arr);
         curr.remove(curr.size() - 1);
-        generate(index + 1, curr, ans, arr);
+
+        int newindex = index + 1;
+        while(newindex < arr.length && arr[newindex] == arr[index]){
+            newindex++;
+        }
+        generate(newindex, curr, ans, arr);
 
     }
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-
         Arrays.sort(nums);
-        HashSet<List<Integer>> set = new HashSet<>();
-        generate(0, new ArrayList<>(), set, nums);
-
-   
-        return new ArrayList<>(set);
+        List<List<Integer>> ans = new ArrayList<>();
+        generate(0, new ArrayList<>(), ans, nums);
+        return ans;
     }
 }
+
+// class Solution {
+
+//     public void generate(int index, List<Integer> curr,    HashSet<List<Integer>> ans, int[] arr) {
+//         if (index == arr.length) {
+//             ans.add(new ArrayList<>(curr));
+//             return;
+//         }
+//         curr.add(arr[index]);
+//         generate(index + 1, curr, ans, arr);
+//         curr.remove(curr.size() - 1);
+//         generate(index + 1, curr, ans, arr);
+
+//     }
+
+//     public List<List<Integer>> subsetsWithDup(int[] nums) {
+
+//         Arrays.sort(nums);
+//         HashSet<List<Integer>> set = new HashSet<>();
+//         generate(0, new ArrayList<>(), set, nums);
+
+//         return new ArrayList<>(set);
+//     }
+// }
