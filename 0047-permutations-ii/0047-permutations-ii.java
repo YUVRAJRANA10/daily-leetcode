c074d1dab189d8f491a02a5c3ea519314dd5cee1
Yuvraj singh <yuvraj.r0810@gmail.com>
2026-09-24 09:52:43 +0530

    Time: 3 ms (93.34%), Space: 46.9 MB (27.66%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,29 +1,34 @@
 class Solution {
-
-    private void backtrack(List<List<Integer>> resultList, ArrayList<Integer> tempList, int[] nums,boolean[] visited) {
+    private void backtrack(List<List<Integer>> resultList, ArrayList<Integer> tempList, int[] nums, boolean[] visited) {
         if (tempList.size() == nums.length) {
-            if(!resultList.contains(tempList)){
-            resultList.add(new ArrayList<>(tempList)); }
+            resultList.add(new ArrayList<>(tempList));
             return;
         }
-        
 
-    for (int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i]) continue;
 
-        if(visited[i])continue;
-        visited[i] = true;
-        tempList.add(nums[i]);
-        backtrack(resultList,tempList,nums,visited);
-        visited[i] = false;
-        tempList.remove(tempList.size()-1);
-    }
+            // Pruning step: Works ONLY if duplicate elements are adjacent
+            if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) continue;
+
+            visited[i] = true;
+            tempList.add(nums[i]);
+
+            backtrack(resultList, tempList, nums, visited);
+
+            visited[i] = false;
+            tempList.remove(tempList.size() - 1);
+        }
     }
 
     public List<List<Integer>> permuteUnique(int[] nums) {
-
         List<List<Integer>> resultList = new ArrayList<>();
         boolean[] visited = new boolean[nums.length];
-        backtrack(resultList, new ArrayList<>(), nums,visited);
+
+        // MUST SORT SO DUPLICATES ARE ADJACENT
+        Arrays.sort(nums);
+
+        backtrack(resultList, new ArrayList<>(), nums, visited);
         return resultList;
     }
 }
